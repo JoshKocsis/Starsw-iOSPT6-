31cbc25328013c0e4527fd20398de4c5d8d77b7a
Josh Kocsis <josh.s.kocsis@gmail.com>
2020-04-23 21:06:22 -0500

    Set up views and connections

--- Starsw (iOSPT6)/ViewController.swift
+++ Starsw (iOSPT6)/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StarsViewController.swift
 //  Starsw (iOSPT6)
 //
 //  Created by Josh Kocsis on 4/23/20.
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class StarsViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var distanceTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+// MARK: IBactions
+    @IBAction func printButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func createButtonTapped(_ sender: Any) {
+    }
+    
+// MARK: UITableViewDataSource
+    
+    
 }
 
+class StarTableViewCell: UITableViewCell {
+    // MARK: IBOutlets
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+}
