e1552e45949a6c25d9d6a16bf14afbf2bfab7af5
Josh Kocsis <josh.s.kocsis@gmail.com>
2020-04-23 20:39:10 -0500

    Initial setup

---
+++ Starsw (iOSPT6)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Starsw (iOSPT6)
+//
+//  Created by Josh Kocsis on 4/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
